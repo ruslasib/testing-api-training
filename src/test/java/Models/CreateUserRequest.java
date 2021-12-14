03c3f9761d5c22fb8f60926250bf78aa9d1ad5c2
Руслан Сибгатуллин <r.sibgatullin@smartcat.ai>
2021-12-14 20:39:19 +0400

    Added jackson, first model and using body in request

---
+++ src/test/java/Models/CreateUserRequest.java
@@ -0,0 +1,48 @@
+package Models;
+
+public class CreateUserRequest {
+
+    public CreateUserRequest(String name1, String gender, String email, String status) {
+        this.name = name1;
+        this.gender = gender;
+        this.email = email;
+        this.status = status;
+    }
+
+    private String name;
+    private String gender;
+    private String email;
+    private String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- src/test/java/Tests/CreateUserTests.java
+++ src/test/java/Tests/CreateUserTests.java
@@ -1,9 +1,11 @@
+package Tests;
+
+import Models.CreateUserRequest;
 import com.github.javafaker.Faker;
 import org.hamcrest.core.IsEqual;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class CreateUserTests {
@@ -12,15 +14,14 @@ public class CreateUserTests {
     public void createUserTest()
     {
         Faker faker = new Faker();
-        String name = String.valueOf(faker.name());
+        String name = faker.name().firstName();
         String email = (name + ".rus@mailinatior.com");
+        CreateUserRequest createUserRequest = new CreateUserRequest(name, "male", email, "active");
 
         given().
                 header("Authorization", "Bearer e739a2ae8d984e9a6f9c35de533e020bfe38263e1f4e8e443c97d8310aa9a9f5").
-                queryParam("name", name.toString()).
-                queryParam("gender", "male").
-                queryParam("email", email.toString()).
-                queryParam("status", "active").
+                contentType("application/json").
+                body(createUserRequest).
         when().
                 post("https://gorest.co.in/public/v1/users").
                 then().
